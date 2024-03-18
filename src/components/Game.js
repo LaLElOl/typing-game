ab98e6b6538c6276b73096e64b44e7f88f7e50ab
Nicolas Bertoni <niculaeb1999@gmail.com>
2024-03-18 16:55:35 -0300

    Armado de finalizacion juego y reset

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,31 @@
-import {Type} from '../components/Type'; 
+import { useState } from 'react';
+import {Type} from '../components/Type';
+import sentencesList from '../resources/sentences.json';
 
 export function Game(){
+
+    const [gameId, setGameId] = useState(1);
+    const [sentence, setSentence] = useState(getRandomSentence(sentencesList));
 
     return (
         <div>
             <p>Type the fastest that you can!!</p>
             <br />
-            <Type />
+            <Type 
+                sentence={sentence}
+                key={gameId} 
+                startNewGame={
+                    () => {
+                        setGameId(gameId + 1);
+                        setSentence(getRandomSentence(sentencesList));
+                    }
+                }
+            />
         </div>
     );
+}
+
+function getRandomSentence(sentences){
+    const index = Math.floor(Math.random() * sentences.length);
+    return sentences[index];
 }
--- src/components/Type.js
+++ src/components/Type.js
@@ -1,36 +1,56 @@
 import {useState} from 'react';
 import './Type.css';
-import sentences from '../resources/sentences.json';
 
-export function Type(){
+export function Type(props){
 
     const [typed, setTyped] = useState('');
     const [inputStyle, setInputStyle] = useState('inputGood');
-    const [sentence, setSentence] = useState(sentences.sentencesList[0]);
-
-    return (
-        <div>
-            <p>{sentences.sentencesList[0]}</p>
+    const [finished, setFinished] = useState(false);
+    const sentence = props.sentence;
+    
+    if(!finished){
+        return (
+            <div>
+            <p>{sentence}</p>
             <input
                 className={inputStyle}
                 value={typed}
                 onChange={e => {
                     setTyped(e.target.value);
                     compareToText(e.target.value, sentence, setInputStyle);
+                    endedTyping(e.target.value, sentence, setFinished);
                 }}
             />
+            </div>
+        );
+    }else{
+    return (
+        <div>
+            <p>Completed correctly!</p>
+            <br></br>
+            <br></br>
+            <div>
+                <p>Want to play again??</p>
+                <button onClick={() => props.startNewGame()}>Play Again</button>
+            </div>
         </div>
     );
+    }
 }
 
 function compareToText(typed, sentence, setInputStyle) {
     
     const lenInput = typed.length;
     const sentenceTrim = sentence.substring(0, lenInput);
-    console.log(sentenceTrim);
     if(sentenceTrim !== typed ){
         setInputStyle('inputWrong');
     }else{
         setInputStyle('inputGood');
+    }
+}
+
+function endedTyping(typed, sentence, setFinished){
+    if(typed === sentence){
+        setFinished(true);
     }
 }
